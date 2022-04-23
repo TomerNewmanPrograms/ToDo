1e01f16d71b6aab372f90ac604a309306a21cc76
TomerNewmanPrograms <tomerne@mta.ac.il>
2022-04-23 21:36:23 +0300

    first files

---
+++ ToDo.js
@@ -0,0 +1,46 @@
+var toggleCheck = function(){
+      let parent = this.parentNode;
+      parent.classList.toggle('checked');
+    }
+    var deleteTodo = function(){
+      let todoLi = this.parentNode;
+      todoLi.remove();
+    };
+    var createTodo = function(todo) {
+      let li = document.createElement('li');
+    
+      let label = document.createElement('label');
+      label.innerHTML = todo;
+      
+      let checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+      checkbox.onclick = toggleCheck;
+    
+      let deleteButton = document.createElement('button');
+      deleteButton.innerHTML = "Delete";
+      deleteButton.className = "delete";
+      deleteButton.onclick = deleteTodo;
+    
+    
+      //nest todo elements in list item
+      li.appendChild(checkbox); 
+      li.appendChild(label);
+      li.appendChild(deleteButton);
+      return li;
+    }
+    
+    document.getElementById('add').onclick = function() {
+        // store the button's parent element (.addTodo <div>) in a variable
+      var parent = this.parentNode;
+      // store the input, which is the *first* child element of the .addTodo <div>
+      var addTextInput = parent.children[0];
+       if(addTextInput.value === "") {
+        return;
+      } else {
+        //add todo
+        createTodo(addTextInput.value);
+        //reset input 
+        addTextInput.value = "";
+      }
+    }
+    
