df9f97157b3056eafdddbd843fe1724e42f80f1a
TomerNewmanPrograms <tomerne@mta.ac.il>
2022-04-28 00:26:50 +0300

    fixing spacing

--- ToDo.js
+++ ToDo.js
@@ -1,21 +1,27 @@
-var toggleCheck = function () {
+var toggleCheck = function ()
+{
     let _parent = this.parentNode;
     _parent.classList.toggle('checked');
 };
 
-var deleteTodo = function () {
+var deleteTodo = function () 
+{
     let todoLi = this.parentNode;
     todoLi.remove();
 };
 
-var createTodo = function (todo) {
+var createTodo = function (todo) 
+{
+    // Create li element
     let li = document.createElement('li');
     let label = document.createElement('label');
-    label.innerHTML = todo;
+    let deleteButton = document.createElement('button');
     let checkbox = document.createElement('input');
+
+    label.innerHTML = todo;
     checkbox.type = "checkbox";
     checkbox.onclick = toggleCheck;
-    let deleteButton = document.createElement('button');
+    
     deleteButton.innerHTML = "Delete";
     deleteButton.className = "delete";
     deleteButton.onclick = deleteTodo;
@@ -26,14 +32,17 @@ var createTodo = function (todo) {
     return li;
 };
 
-document.getElementById('add').onclick = function () {
+document.getElementById('add').onclick = function () 
+{
     var parent = this.parentNode;
     var addTextInput = parent.children[0];
     var ul = document.getElementById("todoList");
-    if (addTextInput.value === "") {
+    if (addTextInput.value === "") 
+    {
         return;
     }
-    else {
+    else 
+    {
         var templi = createTodo(addTextInput.value);
         ul.appendChild(templi);
         addTextInput.value = "";
